a2f2dfbbbff315ef2539e917ce750d079af4dede
John Carney <john@pdfta.com>
2019-03-06 18:06:37 +0000

    Gets data from API in docker.

--- metrolink/main.py
+++ metrolink/main.py
@@ -1,19 +1,28 @@
+import json
 import requests
 
 
-def call_metrolink():
+def call_metrolink(api_key: str):
     url = "https://api.tfgm.com/odata/Metrolinks"
     headers = {
-        'Ocp-Apim-Subscription-Key': "<>",
-        'Authorization': "Bearer <>",
+        'Ocp-Apim-Subscription-Key': "{api_key_value}".format(
+            api_key_value=api_key)
     }
     response = requests.request("GET", url, headers=headers)
     print(response.text)
     return response.text
 
 
+def load_metrolink_api_key() -> dict:
+    with open('/secrets/private-key-metrolink.json') as f:
+        credentials = json.load(f)
+    return credentials
+
+
 def main() -> int:
-    call_metrolink()
+    metrolink_key = load_metrolink_api_key()['primary-key']
+    call_metrolink(metrolink_key)
+    return 0
 
 
 if __name__ == "__main__":
